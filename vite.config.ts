d32a2a65271198a194f1642023ed1770bbe07310
Boris Chubiryaev <104657346+BorisChubiryaev@users.noreply.github.com>
2026-07-30 10:24:43 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+// vite.config.ts
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // Добавляем только это:
+  optimizeDeps: {
+    exclude: ['pdfjs-dist'],
+  },
+});
